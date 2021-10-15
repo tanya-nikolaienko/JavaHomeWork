3bc1d42f075b558cc7b1369c5fa824fb1b0f740c
Tatjana.Nikolaenko <dn301182nti@gmail.com>
2021-10-15 12:02:16 +0300

    init commit

--- src/com/pb/nikolaenko/hw2/interval.java
+++ src/com/pb/nikolaenko/hw2/interval.java
@@ -1,4 +1,26 @@
 package com.pb.nikolaenko.hw2;
 
+import java.util.Scanner;
+
 public class interval {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        System.out.print("Введите целое число: ");
+        x = scan.nextInt();
+        if (x < 0) {
+            System.out.print("Число вне промежутков");
+        } else if (x <= 14) {
+            System.out.print("Число относится к промежутку [0 -14]");
+        } else if (x <= 35) {
+            System.out.print("Число относится к промежутку[15 -35]");
+        } else if (x <= 50) {
+            System.out.print("Число относится к промежутку [36 -50]");
+        }else if (x <= 100) {
+            System.out.print("Число относится к промежутку [51 -100]");
+        }else if (x > 100) {
+            System.out.print("Число вне промежутков");
+        }
+
+    }
 }
