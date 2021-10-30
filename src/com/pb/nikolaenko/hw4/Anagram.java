e16dd60eebf66287f3b67fd5919846cf602dea46
Tatjana.Nikolaenko <dn301182nti@gmail.com>
2021-10-30 22:23:50 +0300

    init commit

--- src/com/pb/nikolaenko/hw4/Anagram.java
+++ src/com/pb/nikolaenko/hw4/Anagram.java
@@ -11,23 +11,29 @@ public class Anagram {
         Scanner input = new Scanner(System.in);
         System.out.print("Введите первую фразу: ");
         String originalF = input.nextLine();//вводим нашу фразу
-
         originalF = originalF.toLowerCase();//приводим в нижний регистр, тк сортировка чуствительна к регистру
-        //System.out.println(originalF);//проверка регистра
-        char[] charsF = originalF.toCharArray();
+
+        String originalF2 = originalF.replaceAll("\\s","");//удаление пробелов
+        String originalF3 = originalF2.replaceAll("[^а-яА-ЯёЁ]", "");//удаление пунктуации
+//        System.out.println("Строка отформатированная: " + originalF3);//выполнения замен пробелов и запятых
+        char[] charsF = originalF3.toCharArray();
         Arrays.sort(charsF);
         String sortedF = new String(charsF);
-          System.out.println(sortedF);
+//        System.out.println(sortedF);
 
 
         System.out.print("Введите вторую фразу: ");
         String originalS = input.nextLine();//вводим нашу фразу
         originalS = originalS.toLowerCase();
-        //System.out.println(originalF);//проверка регистра
-        char[] charsS = originalS.toCharArray();
+
+        String originalS2 = originalS.replaceAll("\\s","");//удаление пробелов
+        String originalS3 = originalS2.replaceAll("[^а-яА-ЯёЁ]","");//удаление пунктуации
+
+        //System.out.println(originalS3);//проверка выполнения замен пробелов и запятых
+        char[] charsS = originalS3.toCharArray();
         Arrays.sort(charsS);
         String sortedS = new String(charsS);
-        System.out.println(sortedS);
+        //System.out.println(sortedS);
 
         boolean test1 = sortedF.equals(sortedS);
 
