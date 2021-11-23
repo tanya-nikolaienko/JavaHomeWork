2db77072ba32c909d3bdb07c9c0d03630c65c3d1
Tatjana.Nikolaenko <dn301182nti@gmail.com>
2021-11-23 20:25:56 +0200

    init commit

--- src/com/pb/nikolaenko/hw9/FileNumbers.java
+++ src/com/pb/nikolaenko/hw9/FileNumbers.java
@@ -17,9 +17,7 @@ public class FileNumbers {
           Random rnd = new Random();
 
         Path path = Paths.get("E:\\numbers.txt");
-        System.out.println("1.1 Создание массива, демонстрация содержимого первого файла:");
-        System.out.println();
-        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            try (BufferedWriter writer = Files.newBufferedWriter(path)) {
 
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
@@ -29,26 +27,22 @@ public class FileNumbers {
 
         for (int i=0;i < a.length;i++,System.out.println()) {
             for (int j=0;j < a[i].length;j++) {
-                System.out.print(a[i][j]+" ");
+                //System.out.print(a[i][j]+" ");
                 writer.write(String.valueOf(a[i][j] +" "));
             }
                 writer.newLine();
 
         }
-
+            System.out.println("1. Файл numbers успешно создан и записан.");
       } catch (Exception ex){
      System.out.println("Error with file write: " + ex);
  }
     }
 
 
-
     static void createOddNumbersFile(){
-        System.out.println();
-        System.out.println("2.1. Результат чтения  первого файла:");
         Path path = Paths.get("E:\\numbers.txt");
         Path path1 = Paths.get("E:\\odd-numbers.txt");
-
 
         try (BufferedReader br = new BufferedReader(new FileReader(String.valueOf(path)));
              BufferedWriter bw = new BufferedWriter(new FileWriter(String.valueOf(path1)))) {
@@ -56,46 +50,28 @@ public class FileNumbers {
             String s;
 
             while ((s = br.readLine()) != null) {
-                bw.write(s);
-                bw.newLine();
 
                 String[] numbersString = s.split(" ");
-                int[] numbers = new int[99];
-                int counter = 0;  // счетчик индекса ячейки
-                for(String number: numbersString){   //циклом переводим стринг в инт
-                    numbers[counter++] = Integer.parseInt(number); // получаем из строки int
-
-                    if (numbers[counter] %2 !=1){
-                        numbers[counter] = 0;
-}
+                for (String numStr : numbersString) {
+                    int number = Integer.parseInt(numStr);
+                    if (number % 2 == 0) {
+                        bw.write("0 ");
+                    } else {
+                        bw.write(numStr + " ");
+                    }
                 }
-
-                System.out.println("second massive"+Arrays.toString(numbers));
-
+                bw.newLine();
+            }
+            System.out.println("2. Файл numbers успешно считан. Файл odd-numbers записан успешно ");
         }
-
-
-
-
+         catch (NoSuchElementException | IOException ex)
+                {
+            System.out.println("Error");
         }
-         catch (NoSuchElementException | IOException ex) {
-            System.out.println("Файл прочитан.");
-        } //catch (IOException ex) {
-            //ex.printStackTrace();
-        }
-
-
-
-
-
+     }
 
     public static void main (String[] args) throws IOException {
         createNumbersFile();
         createOddNumbersFile();
-
-
-
     }
-
-
 }
