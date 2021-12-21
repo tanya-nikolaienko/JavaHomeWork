fd7f4fe1d0f202ee3beeefcf09c7f65f6a4f8c5f
dn301182nti <dn301182nti@gmail.com>
2021-12-21 21:27:08 +0200

    init commit

---
+++ src/com/pb/nikolaenko/hw12/Subscriber.java
@@ -0,0 +1,69 @@
+package com.pb.nikolaenko.hw12;
+
+import java.time.LocalDateTime;
+
+public class Subscriber {
+    private String name;
+    private String birthdate;
+    private String phone;
+    private String address;
+    private LocalDateTime localDateTime;
+
+
+    public Subscriber(String name, String birthdate, String phone, String address, LocalDateTime localDateTime) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.phone = phone;
+        this.address = address;
+        this.localDateTime = localDateTime;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getDatetime() {
+        return localDateTime;
+    }
+
+    public void setDatetime(LocalDateTime datetime) {
+        this.localDateTime = datetime;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Contact [Имя=" + name + ", Дата рождения=" + birthdate + ", Телефон=" + phone + ", Адрес=" + address
+                + ", localDateTime: " + localDateTime + "]";
+    }
+
+}
---
+++ src/com/pb/nikolaenko/hw12/View.java
@@ -0,0 +1,53 @@
+package com.pb.nikolaenko.hw12;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.Scanner;
+
+public class View {
+    public static void main(String[] args) throws JsonProcessingException {
+    PhoneBook phoneBook = new PhoneBook( );
+    Scanner choice = new Scanner (System.in);
+    int input;
+    int count = 0;
+
+    while (count == 0) {
+        System.out.println ("\n------МЕНЮ УПРАВЛЕНИЯ------ \n 1 - добавить контакт;" +
+                "\n 2 - удалить контакт ;\n 3 - поиск контакта; " + "\n 4 - вывести на экран все контакты;"
+                + "\n 5 - редактировать контакт;" + "\n 6 - запись в файл всех данных;"
+                + "\n 7 - загрузка из файла всех данных;" + "\n 8 - выход");
+        System.out.println ("~~~~~" + " Выберите действие (введите цифру)" + "~~~~");
+        input = choice.nextInt ( );
+
+        switch (input) {
+            case 1:
+                phoneBook.add();
+                break;
+            case 2:
+                phoneBook.remove();
+                break;
+            case 3:
+                phoneBook.search();
+                break;
+            case 4:
+                phoneBook.show();
+                break;
+            case 5:
+                phoneBook.edit();
+                break;
+            case 6:
+                phoneBook.createFile();
+                break;
+            case 7:
+                phoneBook.donwloadFile();
+                break;
+            case 8:
+                System.out.println ("Выход!");
+                count++;
+                break;
+
+        }
+
+    }
+}
+}
